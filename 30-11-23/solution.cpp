77f8bc620d4604a3e2c5da7300a02a3d1f6f2313
iamhariharanvj <hariharanvj2003@gmail.com>
2023-11-30 17:05:10 +0530

    30/11/23 - Minimum One Bit Operations to Make Integers Zero

--- 30-11-23/solution.cpp
+++ 30-11-23/solution.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minimumOneBitOperations(int n) {
         int res;
+        
         for (res = 0; n > 0; n &= n - 1)
             res = -(res + (n ^ (n - 1)));
         return abs(res);
