b7e64fd5f16e1e6c4d3f79d1cf5fdb6cc77bc86e
iamhariharanvj <hariharanvj2003@gmail.com>
2023-12-22 21:53:42 +0530

    Maximum Score After Splitting a String

---
+++ 22-12-23/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int max_score = 0;
+        int left_zeros = 0;
+        int right_ones = 0;
+
+        for(char c:s)
+            if(c=='1') right_ones+=1;
+
+        for(int i=0; i<s.length()-1; i++){
+            if(s[i]=='0')left_zeros++;
+            if(s[i]=='1') right_ones--;
+            max_score = max(max_score, left_zeros+right_ones);
+        }
+
+        return max_score;
+    }
+};
